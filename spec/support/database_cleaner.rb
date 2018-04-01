8dc3fe873b325fa932806d5e6bb370a080b276aa
ne4ajb <bogdanlitvin8@gmail.com>
2018-04-01 20:47:32 +0300

    [+] configure rspec & factory & capybara

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+DatabaseCleaner.strategy = :truncation
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+  config.after :each do
+    DatabaseCleaner.clean
+  end
+end
