dc23e474f03c5ea65787cdf5c2c41dc039be3228
ne4ajb <bogdanlitvin8@gmail.com>
2018-04-26 22:06:11 +0300

    [~] refactoring code

--- app/controllers/initiatives_controller.rb
+++ app/controllers/initiatives_controller.rb
@@ -2,7 +2,6 @@
 
 class InitiativesController < ApplicationController
   load_and_authorize_resource find_by: :slug
-  add_breadcrumb I18n.t('views.pages.global.home'), :root_path
   def index
     @initiatives = Initiative.includes(:categories).page(params[:page]).per(5)
     add_breadcrumb I18n.t('views.pages.global.initiatives'), :initiatives_path
