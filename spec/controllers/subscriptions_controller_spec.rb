90de129df052e4e817aec1c8c2d5217ec241aff5
ne4ajb <bogdanlitvin8@gmail.com>
2018-05-01 12:18:39 +0300

    [+] add test for subscription

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -6,10 +6,22 @@ RSpec.describe SubscriptionsController, type: :controller do
   let(:subscription) { create(:subscription) }
 
   describe 'POST #create' do
-    it 'the number of subscriptions should increse' do
-      subscriptions = Subscription.count
-      post :create, params: { subscription: { email: build(:subscription).email } }
-      expect(Subscription.count).to eq(subscriptions + 1)
+    context 'with correct parameters' do
+      it 'the number of subscriptions should increse' do
+        subscriptions = Subscription.count
+        post :create, params: { subscription: build(:subscription).attributes }
+        expect(Subscription.count).to eq(subscriptions + 1)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'with incorrect parameters' do
+      it 'should reneres the new template' do
+        subscriptions = Subscription.count
+        post :create, params: { subscription: { emails: '' } }
+        expect(Subscription.count).to eq(subscriptions)
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 end
