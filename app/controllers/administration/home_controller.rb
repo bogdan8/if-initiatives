10bf85304b8c9b95eb3b4a2123db16d2088ec895
ne4ajb <bogdanlitvin8@gmail.com>
2018-05-10 19:55:54 +0300

    [+] add presentor for administration home index

--- app/controllers/administration/home_controller.rb
+++ app/controllers/administration/home_controller.rb
@@ -3,11 +3,7 @@
 module Administration
   class HomeController < Administration::BaseController
     def index
-      @initiatives_count = Initiative.count
-      @initiatives = Initiative.includes(:categories).without_draft.last(3)
-      @users_count = User.count
-      @subscriptions_count = Subscription.count
-      @categories_count = Category.count
+      @presenter = Administrations::IndexPresenter.new
     end
   end
 end
