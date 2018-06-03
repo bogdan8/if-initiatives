59f69971f0129598d590a571039fb06cbd97da3a
ne4ajb <bogdanlitvin8@gmail.com>
2018-06-03 11:46:58 +0300

    [+] display Initiative

--- client/src/containers/Initiatives.jsx
+++ client/src/containers/Initiatives.jsx
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import InitiativeList from '../components/InitiativeList.jsx'
+import { Grid } from 'react-mdl'
 
-import * as initiativeAction from '../actions/initiativeActions';
+import * as initiativeActions from '../actions/initiativeActions'
 
 class Initiatives extends Component {
   componentWillMount() {
     this.props.actions.allInitiatives();
-  };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (JSON.stringify(nextProps.initiatives) != JSON.stringify(this.props.initiatives))
+  }
 
   render() {
     const { initiatives } = this.props;
+    const listInitiatives = initiatives.map((initiative, index) => {
+      return <InitiativeList initiative={initiative} key={index} />
+    }) 
     
-    return (
-      <div>
-        {initiatives}
-      </div>
-    )
-  };
-};
+    return <Grid>
+      { listInitiatives }
+    </Grid>
+  }
+}
 
 function mapStateToProps(state) {
   return {
@@ -29,7 +36,7 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators({...initiativeActions}, dispatch)
-  };
-};
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Initiatives);
