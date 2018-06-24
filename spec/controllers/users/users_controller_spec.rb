62472df78b0a02807b8c7a009197403db197171b
ne4ajb <bogdanlitvin8@gmail.com>
2018-06-24 14:57:07 +0300

    [+] add missing tests

---
+++ spec/controllers/users/users_controller_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Users::UsersController, type: :controller do
+  let(:user) { create(:user) }
+
+  before(:each) do
+    login_user(user)
+  end
+
+  describe 'GET #show' do
+    it 'renders the template with status' do
+      get :show, params: { id: user }
+      expect(response).to render_template(:show)
+      expect(response.status).to eq(200)
+    end
+  end
+end
