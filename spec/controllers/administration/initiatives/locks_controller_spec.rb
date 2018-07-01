859c6597e119824ea11cb3be148b11119e9cccbd
ne4ajb <bogdanlitvin8@gmail.com>
2018-07-01 20:30:28 +0300

    [+] add test for locks controller

---
+++ spec/controllers/administration/initiatives/locks_controller_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Administration::Initiatives::LocksController, type: :controller do
+  let(:user) { create(:user) }
+  let(:initiative) { create(:initiative, user_id: user.id) }
+
+  before(:each) do
+    login_admin(user)
+  end
+
+  describe 'POST #update' do
+    it 'initiative state should change to locked instead fundraising' do
+      initiative.state = :fundraising
+      initiative.save
+      post :update, params: { id: initiative.slug }
+      expect(Initiative.find(initiative.id).locked?).to eq(true)
+      expect(response).to redirect_to(administration_initiatives_path)
+    end
+
+    it 'initiative state should not change if wrong state' do
+      initiative.state = :implemented
+      initiative.save
+      post :update, params: { id: initiative.slug }
+      expect(Initiative.find(initiative.id).locked?).to eq(false)
+      expect(response).to redirect_to(administration_initiatives_path)
+    end
+  end
+end
