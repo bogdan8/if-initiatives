656688791c112a14d28e5610ab5eb021d0630c99
ne4ajb <bogdanlitvin8@gmail.com>
2018-08-02 22:18:28 +0300

    [~] refactor helper and add new styles

--- app/helpers/role_helper.rb
+++ app/helpers/role_helper.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 module RoleHelper
-  # helper of links for roles
   def roles_links(roles, user)
     links = []
-    classes = 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent font-size-10'
+    classes = 'btn btn-success btn-sm'
     roles.each do |role|
-      text = t("views.pages.global.role.#{role.name}") # localization text
-      role_patch = role_administration_user_path(user, role: role.name) # administration roule path
+      text = t(".role.#{role.name}")
+      role_patch = role_administration_user_path(user, role: role.name)
       links << link_to_if(user.roles.first.name != role.name, text, role_patch, class: classes)
     end
     safe_join(links)
