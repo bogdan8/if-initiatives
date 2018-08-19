70ac0f5351ee929a1c84bec226bff14756fc2c85
ne4ajb <bogdanlitvin8@gmail.com>
2018-08-19 16:29:21 +0300

    [+] add Arcive controller

---
+++ app/controllers/admins/initiatives/archives_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_litera: true
+
+module Admins
+  module Initiatives
+    class ArchivesController < Admins::Initiatives::BaseController
+      def update
+        @initiative = Initiative.friendly.find(params[:id])
+        if @initiative.archive
+          step(@initiative)
+          redirect_to admins_initiatives_path, success: t('.success')
+        else
+          redirect_to admins_initiatives_path, error: @initiative.errors.full_messages.to_sentence
+        end
+      end
+    end
+  end
+end
