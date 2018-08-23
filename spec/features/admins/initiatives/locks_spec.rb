33bb18f17346aa78ad406247844112e4995ca1a7
ne4ajb <bogdanlitvin8@gmail.com>
2018-08-23 20:53:11 +0300

    [+] add feature test for locks

---
+++ spec/features/admins/initiatives/locks_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Locks', type: :feature do
+  let!(:admin) { create(:admin) }
+  let!(:user) { create(:user) }
+  let!(:initiative) { create(:initiative, user_id: user.id) }
+
+  feature '#Update' do
+    scenario 'from fundraising' do
+      initiative.update_column(:state, :fundraising)
+      login_admin_feature(admin)
+      visit admins_initiatives_path
+      click_link I18n.t('admins.initiatives.buttons.lock')
+      expect(find('#alert')).to have_text I18n.t('admins.initiatives.locks.update.success')
+    end
+
+    scenario 'from implementing' do
+      initiative.update_column(:state, :implementing)
+      login_admin_feature(admin)
+      visit admins_initiatives_path
+      click_link I18n.t('admins.initiatives.buttons.lock')
+      expect(find('#alert')).to have_text I18n.t('admins.initiatives.locks.update.success')
+    end
+  end
+end
