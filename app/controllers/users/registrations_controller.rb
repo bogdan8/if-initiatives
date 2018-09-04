74a60c0d2ff07156c770f02691816fa9ec077cfd
ne4ajb <bogdanlitvin8@gmail.com>
2018-09-04 21:30:29 +0300

    [~] fix registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,5 +2,14 @@
 
 module Users
   class RegistrationsController < Devise::RegistrationsController
+    private
+
+    def sign_up_params
+      params.require(:user).permit(:email, :name, :phone, :age, :avatar, :password, :password_confirmation, :current_password)
+    end
+
+    def account_update_params
+      params.require(:user).permit(:email, :name, :phone, :age, :avatar, :password, :password_confirmation, :current_password)
+    end
   end
 end
