25aa74e6526177efd52718b9ddaea4a62ff7a3fd
ne4ajb <bogdanlitvin8@gmail.com>
2018-10-13 19:41:14 +0300

    [~] refactor rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-
-SimpleCov.start do
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Models', 'app/models'
-end
+SimpleCov.start
 
 require 'codecov'
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
