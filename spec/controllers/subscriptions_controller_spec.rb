010be9650cd66cced73f8bf4188c9fa81e72a3e6
ne4ajb <bogdanlitvin8@gmail.com>
2018-10-27 19:25:39 +0300

    [+] add subscriptions task

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-# TODO: need to fix this tests
-# RSpec.describe SubscriptionsController, type: :controller do
-#  let(:subscription) { create(:subscription) }
 
-#  describe 'POST #create' do
-#    context 'with correct parameters' do
-#      it 'the number of subscriptions should increse' do
-#        subscriptions = Subscription.count
-#        post :create, params: { subscription: build(:subscription).attributes }
-#        expect(Subscription.count).to eq(subscriptions + 1)
-#      end
-#    end
+RSpec.describe SubscriptionsController, type: :controller do
+  let(:subscription) { create(:subscription) }
 
-#    context 'with incorrect parameters' do
-#      it 'should renders the new template' do
-#        subscriptions = Subscription.count
-#        post :create, params: { subscription: { emails: '' } }
-#        expect(Subscription.count).to eq(subscriptions)
-#      end
-#    end
-#  end
-# end
+  describe 'POST #create', js: true do
+    context 'with correct parameters' do
+      it 'the number of subscriptions should increse' do
+        subscriptions = Subscription.all.count
+        post :create, params: { subscription: build(:subscription).attributes }, format: :js
+        expect(Subscription.all.count).to eq(subscriptions + 1)
+      end
+    end
+
+    context 'with incorrect parameters' do
+      it 'should renders the new template' do
+        subscriptions = Subscription.all.count
+        post :create, params: { subscription: { emails: '' } }, format: :js
+        expect(Subscription.all.count).to eq(subscriptions)
+      end
+    end
+  end
+end
