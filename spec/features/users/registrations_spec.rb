b4d1d45f514cd103230e38b1e0a04b1a3eb1c5e0
ne4ajb <bogdanlitvin8@gmail.com>
2018-11-25 13:11:15 +0200

    [~] fix tests

--- spec/features/users/registrations_spec.rb
+++ spec/features/users/registrations_spec.rb
@@ -25,7 +25,8 @@ feature 'Registration', type: :feature do
     scenario 'with correct parameters' do
       within '#edit_user' do
         fill_in 'user_email', with: user.email
-        fill_in 'user_name', with: user.name
+        fill_in 'user_first_name', with: user.first_name
+        fill_in 'user_last_name', with: user.last_name
         fill_in 'user_phone', with: user.phone
         fill_in 'user_age', with: user.age
         fill_in 'user_current_password', with: user.password
@@ -37,13 +38,14 @@ feature 'Registration', type: :feature do
     scenario 'with incorrect parameters' do
       within '#edit_user' do
         fill_in 'user_email', with: ''
-        fill_in 'user_name', with: ''
+        fill_in 'user_first_name', with: ''
+        fill_in 'user_last_name', with: ''
         fill_in 'user_phone', with: ''
         fill_in 'user_age', with: ''
         fill_in 'user_current_password', with: ''
       end
       click_button I18n.t('devise.registrations.edit.update')
-      expect(page).to have_text I18n.t('errors.messages.blank'), count: 5
+      expect(page).to have_text I18n.t('errors.messages.blank'), count: 6
     end
   end
 end
